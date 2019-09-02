63c044af626d18b75405c6f410ef6fe494f827f9
zw lin <zhiwen@kooboo.cn>
2019-09-02 22:50:29 +0800

    remove development configuration json file

---
+++ src/api/Doublelives.Core/Configs/TencentCosOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Doublelives.Core.Configs
+{
+    public class TencentCosOptions
+    {
+        public string AppId { get; set; }
+
+        public string SecretId { get; set; }
+
+        public string SecretKey { get; set; }
+
+        public string Bucket { get; set; }
+    }
+}
--- src/api/Doublelives.Core/DIConfig.cs
+++ src/api/Doublelives.Core/DIConfig.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Doublelives.Core
 {
-    class DIConfig
+    public static class DIConfig
     {
+        public static void Configure(IServiceCollection services, IConfiguration configuration)
+        {
+            ConfigureServices();
+        }
+
+        private static void ConfigureServices()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
