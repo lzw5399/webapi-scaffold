8b31c1e85224e21a9a01859b614d03fc14cece3b
zhiwen <zhiwen@kooboo.cn>
2019-09-09 16:08:36 +0800

    add vscode developing file

--- Doublelives.Api/Swagger/SwaggerAddHeaderParameter.cs
+++ Doublelives.Api/Swagger/SwaggerAddHeaderParameter.cs
@@ -1,9 +1,8 @@
-﻿
+﻿using System;
 using Doublelives.Shared.Constants;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Swashbuckle.AspNetCore.Swagger;
 using Swashbuckle.AspNetCore.SwaggerGen;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
